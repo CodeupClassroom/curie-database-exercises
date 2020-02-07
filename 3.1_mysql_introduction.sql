ceeab9a7d65e93b74cf7713038d6a37cec88bce7
Zach Gulde <zachgulde@gmail.com>
2020-02-07 12:14:52 -0600

    Add notes on sequel pro and databases

---
+++ 3.1_mysql_introduction.sql
@@ -0,0 +1,23 @@
+SELECT * FROM mysql.user;
+
+SELECT user, HOST
+FROM mysql.user;
+
+SELECT * FROM mysql.help_topic;
+
+SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;
+
+
+USE mysql;
+
+USE employees; 
+
+SELECT DATABASE();
+
+SELECT * FROM departments;
+
+SELECT * FROM employees.departments;
+
+
+
+
